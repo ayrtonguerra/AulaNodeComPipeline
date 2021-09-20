4140185ce831e010bfe2cbc22284ec1f0eade3ba
Ayrton Rodrigues <ayrtonrodrigues161@gmail.com>
2021-09-19 21:33:57 -0300

    Comentando o teste com erro

--- calculator.test.js
+++ calculator.test.js
@@ -11,6 +11,8 @@ test("testar divisão de 42/6", () => {
     expect(calculator.divisao(42,6)).toBe(7);
 });
 
+/*
 test("testar soma de 5+5", () => {
-    expect(calculator.soma(5,5)).toBe(11); /* Forçando um erro no teste */
+    expect(calculator.soma(5,5)).toBe(11); //Forçando um erro no teste
 });
+*/
